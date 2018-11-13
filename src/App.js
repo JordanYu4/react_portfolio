d97d5aedadcc019cce20613db811cf93d4b483fe
Jordan Yu <jordanyu44@gmail.com>
2018-11-12 18:25:42 -0800

    Break technologies out into component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './stylesheets/App.css';
 
+import Technologies from './javascripts/technologies.jsx';
+import Projects from './javascripts/projects.jsx';
+
 class App extends Component {
   render() {
     return (
@@ -36,151 +39,11 @@ class App extends Component {
           </section>
 
 					<section id="one" class="wrapper style2 spotlights">
-            <section>
-              <a href="#" class="image"><img src="images/exp-dashboard.gif" alt="" data-position="center center" /></a>
-              <div class="content">
-                <div class="inner">
-                  <h2>Experience Premier</h2>
-                  <p>Sentiment analysis for real time speech || MERN stack, React / Redux, MS Azure, IBM Watson</p>
-                  <ul class="actions">
-                    {/* <li><a href="projects.html" class="button">Learn more</a></li> */}
-										<li><a href="https://experience-premier.herokuapp.com"
-                      class="button" target="_blank">Live site</a></li>
-                  </ul>
-                </div>
-              </div>
-            </section>
-            <section>
-              <a href="#" class="image"><img src="images/yelp-helper.gif" alt="" data-position="top center" /></a>
-              <div class="content">
-                <div class="inner">
-                  <h2>Yelp-Helper</h2>
-                  <p>Data visualization tool for making faster decisions || JavaScript, Express, Node, Yelp Fusion, Chart.js</p>
-                  <ul class="actions">
-                    {/* <li><a href="projects.html" class="button">Learn more</a></li> */}
-										<li><a href="https://yelp-helper.herokuapp.com"
-                      class="button" target="_blank">Live site</a></li>
-                  </ul>
-                </div>
-              </div>
-            </section>
-            <section>
-              <a href="#" class="image"><img src="images/gylio-dashboard.gif" alt="" data-position="25% 25%" /></a>
-              <div class="content">
-                <div class="inner">
-                  <h2>Gylio</h2>
-                  <p>Get Your Life in Order: full-stack Trello clone || Ruby on Rails, React / Redux, PostgreSQL</p>
-                  <ul class="actions">
-                    {/* <li><a href="projects.html" class="button">Learn more</a></li> */}
-										<li><a href="https://gylio.herokuapp.com"
-                      class="button" target="_blank">Live site</a></li>
-                  </ul>
-                </div>
-              </div>
-            </section>
+            <Projects />
           </section>
 
 					<section id="two" class="wrapper style3 fade-up">
-            <div class="inner">
-              <h2>Technologies</h2>
-              <p>Constantly sharpening my skills and always hungry to learn more.</p>
-              <div class="features">
-                <section>
-                  <i class="major icon fab fa-code"></i>
-                  {/* <i class="major icon fab fa-js"></i> */}
-									<h3>JavaScript</h3>
-                </section>
-                <section>
-                  {/* <i class="major fab fa-react"></i> */}
-									<i class="icon major fab fa-code"></i>
-                  <h3>React / Redux</h3>
-                </section>
-                <section>
-                  <i class="icon major fa-diamond"></i>
-                  <h3>Ruby on Rails</h3>
-                </section>
-                <section>
-                  {/* <i class="major fab fa-node"></i> */}
-									<i class="icon major fab fa-code"></i>
-                  <h3>Node</h3>
-                </section>
-                <section>
-                  <i class="icon major fas fa-database"></i>
-                  <h3>MongoDB</h3>
-                </section>
-                <section>
-                  <i class="icon major fas fa-database"></i>
-                  <h3>PostgreSQL</h3>
-                </section>
-                <section>
-                  {/* <i class="icon major fab fa-aws"></i> */}
-									<i class="icon major fas fa-database"></i>
-                  <h3>AWS</h3>
-                </section>
-                <section>
-                  <i class="icon major fab fa-html5"></i>
-                  <h3>HTML5</h3>
-                </section>
-                <section>
-                  <i class="icon major fab fa-css3"></i>
-                  <h3>CSS3</h3>
-                </section>
-                <section>
-                  {/* <i class="major fab fa-sass"></i> */}
-									<i class="icon major fab fa-code"></i>
-                  <h3>Sass</h3>
-                </section>
-                <section>
-                  <i class="icon major fa-code"></i>
-                  <h3>jQuery</h3>
-                </section>
-                <section>
-                  <i class="icon major fa-chain"></i>
-                  <h3>Webpack</h3>
-                </section>
-                <section>
-                  <i class="icon major fa-code"></i>
-                  <h3>Jest</h3>
-                </section>
-                <section>
-                  <i class="icon major fa-diamond"></i>
-                  <h3>RSpec</h3>
-                </section>
-                <section>
-                  <i class="icon major fa-diamond"></i>
-                  <h3>Capybara</h3>
-                </section>
-                <section>
-                  <i class="icon major fas fa-code-branch"></i>
-                  <h3>Git</h3>
-                </section>
-                <section>
-                  <i class="icon major fa-code"></i>
-                  <h3>Python</h3>
-                </section>
-                <section>
-                  <i class="icon major fa-code"></i>
-                  <h3>SQL</h3>
-                </section>
-                {/* <section>
-                  <i class="icon major fa-code"></i>
-                  <h3>Google Analytics</h3>
-                </section>
-                <section>
-                  <i class="icon major fa-code"></i>
-                  <h3>Adobe Illustrator</h3>
-                </section> */}
-							</div>
-              <ul class="actions">
-                <li>
-                  <a href="https://docs.google.com/document/export?format=pdf&id=1bQYx9kYCyVcyAL8kPN1uz7te7HbRBeTtu2X1uZ0eUms"
-                    class="button"
-                  >
-                    Download Resume
-									</a>
-                </li>
-              </ul>
-            </div>
+            <Technologies />
           </section>
 
 					<section id="three" class="wrapper style2 fade-up">
---
+++ src/javascripts/technologies.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+const Technologies = () => (
+  <div class="inner">
+    <h2>Technologies</h2>
+    <p>Constantly sharpening my skills and always hungry to learn more.</p>
+    <div class="features">
+      <section>
+        <i class="major icon fab fa-code"></i>
+        {/* <i class="major icon fab fa-js"></i> */}
+        <h3>JavaScript</h3>
+      </section>
+      <section>
+        {/* <i class="major fab fa-react"></i> */}
+        <i class="icon major fab fa-code"></i>
+        <h3>React / Redux</h3>
+      </section>
+      <section>
+        <i class="icon major fa-diamond"></i>
+        <h3>Ruby on Rails</h3>
+      </section>
+      <section>
+        {/* <i class="major fab fa-node"></i> */}
+        <i class="icon major fab fa-code"></i>
+        <h3>Node</h3>
+      </section>
+      <section>
+        <i class="icon major fas fa-database"></i>
+        <h3>MongoDB</h3>
+      </section>
+      <section>
+        <i class="icon major fas fa-database"></i>
+        <h3>PostgreSQL</h3>
+      </section>
+      <section>
+        {/* <i class="icon major fab fa-aws"></i> */}
+        <i class="icon major fas fa-database"></i>
+        <h3>AWS</h3>
+      </section>
+      <section>
+        <i class="icon major fab fa-html5"></i>
+        <h3>HTML5</h3>
+      </section>
+      <section>
+        <i class="icon major fab fa-css3"></i>
+        <h3>CSS3</h3>
+      </section>
+      <section>
+        {/* <i class="major fab fa-sass"></i> */}
+        <i class="icon major fab fa-code"></i>
+        <h3>Sass</h3>
+      </section>
+      <section>
+        <i class="icon major fa-code"></i>
+        <h3>jQuery</h3>
+      </section>
+      <section>
+        <i class="icon major fa-chain"></i>
+        <h3>Webpack</h3>
+      </section>
+      <section>
+        <i class="icon major fa-code"></i>
+        <h3>Jest</h3>
+      </section>
+      <section>
+        <i class="icon major fa-diamond"></i>
+        <h3>RSpec</h3>
+      </section>
+      <section>
+        <i class="icon major fa-diamond"></i>
+        <h3>Capybara</h3>
+      </section>
+      <section>
+        <i class="icon major fas fa-code-branch"></i>
+        <h3>Git</h3>
+      </section>
+      <section>
+        <i class="icon major fa-code"></i>
+        <h3>Python</h3>
+      </section>
+      <section>
+        <i class="icon major fa-code"></i>
+        <h3>SQL</h3>
+      </section>
+      {/* <section>
+                  <i class="icon major fa-code"></i>
+                  <h3>Google Analytics</h3>
+                </section>
+                <section>
+                  <i class="icon major fa-code"></i>
+                  <h3>Adobe Illustrator</h3>
+                </section> */}
+    </div>
+    <ul class="actions">
+      <li>
+        <a href="https://docs.google.com/document/export?format=pdf&id=1bQYx9kYCyVcyAL8kPN1uz7te7HbRBeTtu2X1uZ0eUms"
+          class="button"
+        >
+          Download Resume
+									</a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Technologies;
