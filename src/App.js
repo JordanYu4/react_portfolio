a49bf3fd2ca51eb19d230cb53177452876ad4240
Jordan Yu <jordanyu44@gmail.com>
2018-11-12 18:31:29 -0800

    Break contact section out into component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './stylesheets/App.css';
 import Technologies from './javascripts/technologies.jsx';
 import Projects from './javascripts/projects.jsx';
 import HardwarePrototypingPreview from './javascripts/hardware_prototyping_preview.jsx';
+import Contact from './javascripts/contact.jsx';
 
 class App extends Component {
   render() {
@@ -52,46 +53,7 @@ class App extends Component {
           </section>
 
 					<section id="four" class="wrapper style1 fade-up">
-            <div class="inner">
-              <h2>Get in touch</h2>
-              <p>I'm currently looking for my next challenge in software development. Feel free to reach out!</p>
-              <div class="split style1">
-                <section>
-                  <ul class="contact">
-                    <li>
-                      <a href="https://docs.google.com/document/export?format=pdf&id=1bQYx9kYCyVcyAL8kPN1uz7te7HbRBeTtu2X1uZ0eUms"
-                        class="button"
-                      >
-                        Download Resume
-											</a>
-                    </li>
-                    <li>
-                      <h3>Phone</h3>
-                      <span>(617) 637-5420</span>
-                    </li>
-                    <li>
-                      <h3>Social</h3>
-                      <ul class="icons">
-                        <li>
-                          <a href="https://github.com/JordanYu4"
-                            class="fa-github"
-                            target="_blank">
-                            <span class="label">GitHub</span>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="https://www.linkedin.com/in/jordan-yu-4400/"
-                            class="fa-linkedin"
-                            target="_blank">
-                            <span class="label">LinkedIn</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-            </div>
+            <Contact />
           </section>
 
         </div>
---
+++ src/javascripts/contact.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Contact = () => (
+  <div class="inner">
+    <h2>Get in touch</h2>
+    <p>I'm currently looking for my next challenge in software development. Feel free to reach out!</p>
+    <div class="split style1">
+      <section>
+        <ul class="contact">
+          <li>
+            <a href="https://docs.google.com/document/export?format=pdf&id=1bQYx9kYCyVcyAL8kPN1uz7te7HbRBeTtu2X1uZ0eUms"
+              class="button"
+            >
+              Download Resume
+											</a>
+          </li>
+          <li>
+            <h3>Phone</h3>
+            <span>(617) 637-5420</span>
+          </li>
+          <li>
+            <h3>Social</h3>
+            <ul class="icons">
+              <li>
+                <a href="https://github.com/JordanYu4"
+                  class="fa-github"
+                  target="_blank">
+                  <span class="label">GitHub</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/jordan-yu-4400/"
+                  class="fa-linkedin"
+                  target="_blank">
+                  <span class="label">LinkedIn</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+    </div>
+  </div>
+);
+
+export default Contact;
